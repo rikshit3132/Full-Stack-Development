687f90da218bc8dfc754bd2acd3b356676e0e069
= <narsethrikshit@gmail.com>
2026-01-30 19:45:27 +0530

    border motion changed

--- JS-Learning/Machine -coding/class-3/countdown_timer/index.js
+++ JS-Learning/Machine -coding/class-3/countdown_timer/index.js
@@ -1,4 +1,12 @@
+let borderFlowInterval = null;
+let borderStep = 0;
 
+const borderColors = [
+  "rgb(75,139,207)", // top
+  "#76c893", // right
+  "#a594f9", // bottom
+  "#dd2d4a", // left
+];
 
 const btnStart = document.querySelector("#start");
 const btnPause = document.querySelector("#pause");
@@ -44,7 +52,7 @@ const textEl = document.querySelector(".texts");
 let index = 0;
 const divider1 = document.querySelector(".divider1_input");
 const divider2 = document.querySelector(".divider2_input");
-btnStart.addEventListener("click",()=>{
+btnStart.addEventListener("click", () => {
   // validation,transformation
   const mins = parseInt(minsInput.value) || 0;
   const hrs = parseInt(hrsInput.value) || 0;
@@ -66,7 +74,7 @@ btnStart.addEventListener("click",()=>{
   container.style.borderColor = "rgb(75, 139, 207)";
   container.style.boxShadow = "0 0 20px rgba(75,139,207,0.7)";
   defineInterval();
- motivations.style.boxShadow = `
+  motivations.style.boxShadow = `
   0 0 5px   rgb(75, 139, 207),
   0 0 15px  rgb(75, 139, 207),
   0 0 30px  rgb(75, 139, 207),
@@ -77,16 +85,17 @@ btnStart.addEventListener("click",()=>{
   minsInput.style.color = "rgb(75, 139, 207)";
   secsInput.style.color = "rgb(75, 139, 207)";
   divider1.style.color = "rgb(75, 139, 207)";
-   divider2.style.color = "rgb(75, 139, 207)";
+  divider2.style.color = "rgb(75, 139, 207)";
+  startBorderFlow();
 });
-btnPause.addEventListener("click", ()=>{
-  
+btnPause.addEventListener("click", () => {
+  stopBorderFlow();
   btnPause.style.display = "none";
   btnContinue.style.display = "block";
   container.style.borderColor = "#a594f9";
   container.style.boxShadow = "0 0 20px rgba(165,148,249,0.7)";
 
- motivations.style.boxShadow = `
+  motivations.style.boxShadow = `
   0 0 5px   #a594f9,
   0 0 15px  #a594f9,
   0 0 30px  #a594f9,
@@ -96,34 +105,36 @@ btnPause.addEventListener("click", ()=>{
   hrsInput.style.color = "#a594f9";
   minsInput.style.color = "#a594f9";
   secsInput.style.color = "#a594f9";
-   divider1.style.color = "#a594f9";
-   divider2.style.color = "#a594f9";
-   clearInterval(counterId);
+  divider1.style.color = "#a594f9";
+  divider2.style.color = "#a594f9";
+  clearInterval(counterId);
 });
 
-btnContinue.addEventListener("click",()=>{
-    timer(saveTimeLeft);
-    btnContinue.style.display = "none";
-    btnPause.style.display = "block";
-    container.style.borderColor = "#76c893";
-    container.style.boxShadow = "0 0 20px rgba(118,200,147,0.7)";
- motivations.style.boxShadow = `
+btnContinue.addEventListener("click", () => {
+  timer(saveTimeLeft);
+  startBorderFlow();
+  btnContinue.style.display = "none";
+  btnPause.style.display = "block";
+  container.style.borderColor = "#76c893";
+  container.style.boxShadow = "0 0 20px rgba(118,200,147,0.7)";
+  motivations.style.boxShadow = `
   0 0 5px   #76c893,
   0 0 15px  #76c893,
   0 0 30px  #76c893,
   0 0 60px  #76c893
 `;
 
-     motivations.style.color = "#76c893";
-        hrsInput.style.color = "#76c893";
-        minsInput.style.color = "#76c893";
-        secsInput.style.color = "#76c893";
-        divider1.style.color = "#76c893";
-        divider2.style.color = "#76c893";
+  motivations.style.color = "#76c893";
+  hrsInput.style.color = "#76c893";
+  minsInput.style.color = "#76c893";
+  secsInput.style.color = "#76c893";
+  divider1.style.color = "#76c893";
+  divider2.style.color = "#76c893";
 });
-btnReset.addEventListener("click",() =>{
+btnReset.addEventListener("click", () => {
   saveTimeLeft = 0;
   clearInterval(counterId);
+  stopBorderFlow();
   currentMusicIndex = 0;
   secsInput.value = "00";
   minsInput.value = "00";
@@ -133,52 +144,54 @@ btnReset.addEventListener("click",() =>{
   btnStart.style.display = "block";
   btnReset.style.display = "block";
   clearInterval(startInterval);
-    minsInput.style.color = "#dd2d4a";
-    secsInput.style.color = "#dd2d4a";
-    hrsInput.style.color = "#dd2d4a";
+  minsInput.style.color = "#dd2d4a";
+  secsInput.style.color = "#dd2d4a";
+  hrsInput.style.color = "#dd2d4a";
   container.style.boxShadow = "0 0 20px rgba(221,45,74,0.7)";
   motivations.textContent = "";
- divider1.style.color = "#dd2d4a";
+  divider1.style.color = "#dd2d4a";
   divider2.style.color = "#dd2d4a";
   musicStart.style.display = "block";
   musicStop.style.display = "none";
   isMusicMuted = false;
-  stopMusicRotation(); 
-  musicIntervalId = null; 
- bgMusic.pause();
- bgMusic.currentTime = 0;
- bgMusic.src = "";
+  stopMusicRotation();
+  musicIntervalId = null;
+  bgMusic.pause();
+  bgMusic.currentTime = 0;
+  bgMusic.src = "";
 });
-musicStart.addEventListener("click",()=>{
-    if(counterId){
-        isMusicMuted = false;  
-musicStart.style.display = "none";
-musicStop.style.display = "block";
-motivations.style.borderColor = "red";
-motivations.style.boxShadow = `
+musicStart.addEventListener("click", () => {
+  if (counterId) {
+     startBorderFlow();
+    isMusicMuted = false;
+    musicStart.style.display = "none";
+    musicStop.style.display = "block";
+    motivations.style.borderColor = "red";
+    motivations.style.boxShadow = `
   0 0 5px   #76c893,
   0 0 15px  #76c893,
   0 0 30px  #76c893,
   0 0 60px  rgba(118, 200, 147, 0.9)
 `;
 
-motivations.style.color = "#76c893";
-container.style.borderColor = "#76c893";
-container.style.boxShadow = "0 0 20px #76c893";
- hrsInput.style.color = "#76c893";
- minsInput.style.color = "#76c893";
- secsInput.style.color = "#76c893";
- divider1.style.color = "#76c893";
-   divider2.style.color = "#76c893";
-  startMusicRotation();
-    }
+    motivations.style.color = "#76c893";
+    container.style.borderColor = "#76c893";
+    container.style.boxShadow = "0 0 20px #76c893";
+    hrsInput.style.color = "#76c893";
+    minsInput.style.color = "#76c893";
+    secsInput.style.color = "#76c893";
+    divider1.style.color = "#76c893";
+    divider2.style.color = "#76c893";
+    startMusicRotation();
+  }
 });
-musicStop.addEventListener("click",()=>{
-    if(counterId){
+musicStop.addEventListener("click", () => {
+  if (counterId) {
+    stopBorderFlow();
     musicStart.style.display = "block";
     musicStop.style.display = "none";
     motivations.style.borderColor = "red";
-motivations.style.boxShadow = `
+    motivations.style.boxShadow = `
   0 0 5px   red,
   0 0 15px  red,
   0 0 30px  red,
@@ -187,26 +200,25 @@ motivations.style.boxShadow = `
     motivations.style.color = "red";
     container.style.borderColor = "red";
     container.style.boxShadow = "0 0 20px red";
-     hrsInput.style.color = "red";
-     minsInput.style.color = "red";
-     secsInput.style.color = "red";  
-     divider1.style.color = "red";
-     divider2.style.color = "red";
+    hrsInput.style.color = "red";
+    minsInput.style.color = "red";
+    secsInput.style.color = "red";
+    divider1.style.color = "red";
+    divider2.style.color = "red";
 
-     stopMusicRotation();
-    }
-})
+    stopMusicRotation();
+  }
+});
 
 container.addEventListener("mouseenter", () => {
   container.style.borderBottomColor = "#76c893";
-    container.style.boxShadow = "0 12px 20px rgba(118,200,147,0.7)";
-    container.style.borderLeftColor = "#a594f9";
-    container.style.boxShadow = "-12px 0 20px rgba(165,148,249,0.7)";
-    container.style.borderRightColor = "#dd2d4a";
-    container.style.boxShadow = "12px 0 20px rgba(221,45,74,0.7)";
-    container.style.borderTopColor = "rgb(75, 139, 207)";
-    container.style.boxShadow = "0 -12px 20px rgba(75,139,207,0.7)";
-
+  container.style.boxShadow = "0 12px 20px rgba(118,200,147,0.7)";
+  container.style.borderLeftColor = "#a594f9";
+  container.style.boxShadow = "-12px 0 20px rgba(165,148,249,0.7)";
+  container.style.borderRightColor = "#dd2d4a";
+  container.style.boxShadow = "12px 0 20px rgba(221,45,74,0.7)";
+  container.style.borderTopColor = "rgb(75, 139, 207)";
+  container.style.boxShadow = "0 -12px 20px rgba(75,139,207,0.7)";
 });
 container.addEventListener("mouseleave", () => {
   container.style.borderBottomColor = "#76c893";
@@ -218,9 +230,9 @@ container.addEventListener("mouseleave", () => {
   container.style.borderTopColor = "rgb(75, 139, 207)";
   container.style.boxShadow = "0 -12px 20px rgba(75,139,207,0.7)";
 });
-motivations.addEventListener("mouseover",()=>{
-motivations.style.width = "43rem";
-motivations.style.fontSize = "2.5rem";
+motivations.addEventListener("mouseover", () => {
+  motivations.style.width = "43rem";
+  motivations.style.fontSize = "2.5rem";
 });
 motivations.addEventListener("mouseleave", () => {
   motivations.style.width = "33rem";
@@ -230,7 +242,7 @@ motivations.addEventListener("mouseleave", () => {
 function playCurrentMusic() {
   bgMusic.pause();
   bgMusic.src = musicList[currentMusicIndex];
-  bgMusic.load(); 
+  bgMusic.load();
   bgMusic.currentTime = 0;
   bgMusic.play().catch(() => {
     console.log("User interaction required");
@@ -256,69 +268,73 @@ function startMusicRotation() {
 }
 function stopMusicRotation() {
   clearInterval(musicIntervalId);
-  musicIntervalId = null; 
+  musicIntervalId = null;
   bgMusic.pause();
   bgMusic.currentTime = 0;
 }
-function timer(countDownTime){
-counterId = setInterval(() =>{
-updateUIEverySec(countDownTime);
-countDownTime--;
-saveTimeLeft = countDownTime;
-},1000);
+function timer(countDownTime) {
+  counterId = setInterval(() => {
+    updateUIEverySec(countDownTime);
+    countDownTime--;
+    saveTimeLeft = countDownTime;
+  }, 1000);
 }
-function updateUIEverySec(countDownTime){
-    if(countDownTime === 0){
-      clearInterval(counterId);
-     stopMusicRotation(); 
-     currentMusicIndex = 0;
-      bgMusic.src = "";
-      secsInput.value = "00";
-      minsInput.value = "00";
-      hrsInput.value = "00";
-      btnContinue.style.display = "none";
-      btnPause.style.display = "none";
-      btnStart.style.display = "block";
-      btnReset.style.display = "block";
-      musicStart.style.display = "block";
-      musicStop.style.display = "none";
-      return;
-    }
-     let hrs = Math.floor(countDownTime / 3600);
-     let mins = Math.floor((countDownTime % 3600) / 60);
-     let secs = countDownTime % 60;
-    if(secs > 0){
-        secs--;
-        secsInput.value = secs < 10 ? `0${secs}` : `${secs}`;
-        minsInput.value = mins;
-        hrsInput.value = hrs;
-        return;
-    }
-    if(mins > 0){
-        mins--;
-        minsInput.value = mins < 10 ? `0${mins}` : `${mins}`;
-        secsInput.value = 59;
-        hrsInput.value = hrs;
-        return;
-    }
-    if(hrs > 0){
-        hrs--;
-        minsInput.value = 59;
-        secsInput.value = 59;
-        hrsInput.value = hrs < 10 ? `0${hrs}` : `${hrs}`;
-        return;
-    }
+function updateUIEverySec(countDownTime) {
+  if (countDownTime === 0) {
+    clearInterval(counterId);
+    stopMusicRotation();
+    currentMusicIndex = 0;
+    bgMusic.src = "";
+    secsInput.value = "00";
+    minsInput.value = "00";
+    hrsInput.value = "00";
+    btnContinue.style.display = "none";
+    btnPause.style.display = "none";
+    btnStart.style.display = "block";
+    btnReset.style.display = "block";
+    musicStart.style.display = "block";
+    musicStop.style.display = "none";
+    return;
+  }
+  let hrs = Math.floor(countDownTime / 3600);
+  let mins = Math.floor((countDownTime % 3600) / 60);
+  let secs = countDownTime % 60;
+  if (secs > 0) {
+    secs--;
+    secsInput.value = secs < 10 ? `0${secs}` : `${secs}`;
+    minsInput.value = mins;
+    hrsInput.value = hrs;
+    return;
+  }
+  if (mins > 0) {
+    mins--;
+    minsInput.value = mins < 10 ? `0${mins}` : `${mins}`;
+    secsInput.value = 59;
+    hrsInput.value = hrs;
+    return;
+  }
+  if (hrs > 0) {
+    hrs--;
+    minsInput.value = 59;
+    secsInput.value = 59;
+    hrsInput.value = hrs < 10 ? `0${hrs}` : `${hrs}`;
+    return;
+  }
 }
-function validateInputs(mins,hrs,secs){
-    if(mins === 0 && hrs === 0 && secs === 0){
-        alert("please enter valid time");
-        return false;
-    }
-    if(hrs > 24 || hrs < 0 || secs < 0 || mins < 0){
-        alert("Hours more than 24hrs/negatives not allowed");
-        return false;
-    }
-  const{transformmins,transformhrs,transformsecs} =  transformInputs(mins,hrs,secs);
+function validateInputs(mins, hrs, secs) {
+  if (mins === 0 && hrs === 0 && secs === 0) {
+    alert("please enter valid time");
+    return false;
+  }
+  if (hrs > 24 || hrs < 0 || secs < 0 || mins < 0) {
+    alert("Hours more than 24hrs/negatives not allowed");
+    return false;
+  }
+  const { transformmins, transformhrs, transformsecs } = transformInputs(
+    mins,
+    hrs,
+    secs,
+  );
   const totalTime = transformhrs * 60 * 60 + transformmins * 60 + transformsecs;
   if (totalTime > 86400) {
     alert("total time > 24hrs not allowed");
@@ -326,26 +342,65 @@ function validateInputs(mins,hrs,secs){
   }
   return true;
 }
-function transformInputs(mins,hrs,secs){
-    if(secs >= 60){
-        mins++;
-        secs = secs % 60;
-    }
-    if(mins >= 60){
-        hrs++;
-        mins = mins % 60;
-    }
-    return{
-        transformsecs : secs,
-        transformmins: mins,
-        transformhrs : hrs
-    }
+function transformInputs(mins, hrs, secs) {
+  if (secs >= 60) {
+    mins++;
+    secs = secs % 60;
+  }
+  if (mins >= 60) {
+    hrs++;
+    mins = mins % 60;
+  }
+  return {
+    transformsecs: secs,
+    transformmins: mins,
+    transformhrs: hrs,
+  };
 }
 
-function defineInterval(){
-    motivations.textContent = texts[index];
+function defineInterval() {
+  motivations.textContent = texts[index];
   startInterval = setInterval(() => {
     index = (index + 1) % texts.length;
     textEl.textContent = texts[index];
-  },10000); 
+  }, 10000);
+}
+
+function startBorderFlow() {
+  stopBorderFlow(); // safety
+
+  borderFlowInterval = setInterval(() => {
+    // reset all sides
+    container.style.borderTopColor = "transparent";
+    container.style.borderRightColor = "transparent";
+    container.style.borderBottomColor = "transparent";
+    container.style.borderLeftColor = "transparent";
+
+    // activate ONE side at a time
+    switch (borderStep) {
+      case 0:
+        container.style.borderTopColor = borderColors[0];
+        container.style.boxShadow = "0 -8px 20px rgba(75,139,207,0.7)";
+        break;
+      case 1:
+        container.style.borderRightColor = borderColors[1];
+        container.style.boxShadow = "8px 0 20px rgba(118,200,147,0.7)";
+        break;
+      case 2:
+        container.style.borderBottomColor = borderColors[2];
+        container.style.boxShadow = "0 8px 20px rgba(165,148,249,0.7)";
+        break;
+      case 3:
+        container.style.borderLeftColor = borderColors[3];
+        container.style.boxShadow = "-8px 0 20px rgba(221,45,74,0.7)";
+        break;
+    }
+
+    borderStep = (borderStep + 1) % 4;
+  }, 300); // speed (lower = faster)
+}
+
+function stopBorderFlow() {
+  clearInterval(borderFlowInterval);
+  borderFlowInterval = null;
 }
